2ff4b25d6a4c42c5668c8bbc7dc277c2062fba32
wuwei01 <969502105@qq.com>
2021-05-13 09:53:51 +0800

    添加枚举工具类获取枚举信息

---
+++ src/main/java/io/github/wuwei01/utils/EnumImpl.java
@@ -0,0 +1,24 @@
+package io.github.wuwei01.utils;
+
+/**
+ * @ClassName: EnumImpl
+ * @Description: 枚举基本数据类
+ * @Author: wuwei
+ * @Date: 2020/4/29 10:09
+ * @Version: 1.0
+ */
+public interface EnumImpl {
+    /**
+     * 获取枚举code
+     *
+     * @return
+     */
+    String getCode();
+
+    /**
+     * 获取枚举code对应的描述
+     *
+     * @return
+     */
+    String getDesc();
+}
---
+++ src/main/java/io/github/wuwei01/utils/EnumUtil.java
@@ -0,0 +1,24 @@
+package io.github.wuwei01.utils;
+
+import org.springframework.util.StringUtils;
+
+/**
+ * @ClassName: EnumUtil
+ * @Description: 枚举工具类，根据枚举key获取value
+ * @Author: wuwei
+ * @Date: 2020/4/29 10:07
+ * @Version: 1.0
+ */
+public class EnumUtil {
+    public static <T extends EnumImpl> String getCodeDesc(Class<T> clazz, String code) {
+        if (StringUtils.isEmpty(code)) {
+            return null;
+        }
+        for (T _enum : clazz.getEnumConstants()) {
+            if (_enum.getCode().trim().equalsIgnoreCase(code)) {
+                return _enum.getDesc();
+            }
+        }
+        return null;
+    }
+}
